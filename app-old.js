799769aeaca21266d2262acf2d5fa3d31832e3f3
Fede <cfrfido@gmail.com>
2020-12-27 19:12:44 +0100

    Primer commit

---
+++ app-old.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+
+        res.writeHead(200, { 'Content-type': 'application/json' });
+
+        let salida = {
+            nombre: 'Fede Ríos',
+            edad: 38,
+            url: req.url
+        };
+        res.write(JSON.stringify(salida));
+        res.end();
+
+    })
+    .listen(8080);
+
+console.log('Escuchando puerto 8080');
